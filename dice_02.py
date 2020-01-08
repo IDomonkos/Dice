1d3a6a1d999805d6662b3beb4bbb87037651cb09
Sasa Irlanda <sasa.irlanda@gmail.com>
2020-01-08 20:50:12 +0100

    kiértékelést kiraktuk függvénybe

--- dice_02.py
+++ dice_02.py
@@ -16,6 +16,14 @@ layout = [[sg.Slider(range=(1, 10), orientation='h', size=(34, 20), default_valu
 
 window = sg.Window('Dice roller', layout, resizable=True)
 
+
+def evaluate_success(target, count):
+    if target <= count:
+        return 'success'
+    else:
+        return'failure'
+
+
 while True:
     event, values = window.read()
     if event in (None, 'Exit'):
@@ -30,13 +38,9 @@ while True:
 
         success = rolls.count(5) + rolls.count(6)
         target = values['SLIDER']
-        print(f'Target: {target} - Success: {success}')
-        evaluation = ''
-        if target <= success:
-            evaluation = 'success'
-        else:
-            evaluation = 'failure'
 
-        window['TEXT'].update(f'{evaluation} - {rolls}')
+        evaluation = evaluate_success(target, success)
+
+        window['TEXT'].update(f'{evaluation}({success}) - {rolls}')
 
 window.close()
